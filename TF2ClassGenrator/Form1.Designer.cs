37df357c5ac39bc063c6bc612201d7606a5c37f1
skuly <skuly@GLADOS>
2022-06-06 21:40:14 +0300

    Add project files.

---
+++ TF2ClassGenrator/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace TF2ClassGenrator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PrimaryImage = new System.Windows.Forms.PictureBox();
+            this.SecondaryImage = new System.Windows.Forms.PictureBox();
+            this.MeleeImage = new System.Windows.Forms.PictureBox();
+            this.ExtraPicture = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PrimaryImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondaryImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MeleeImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExtraPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 797);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "GenerateButton";
+            this.button2.Text = "Generate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(39)))), ((int)(((byte)(36)))));
+            this.label1.Font = new System.Drawing.Font("TF2 Build", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(122, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Class";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("TF2 Build", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(112, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Primary";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("TF2 Build", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(112, 394);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Secondary";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("TF2 Build", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Yellow;
+            this.label4.Location = new System.Drawing.Point(112, 586);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Melee";
+            // 
+            // PrimaryImage
+            // 
+            this.PrimaryImage.Location = new System.Drawing.Point(97, 68);
+            this.PrimaryImage.Name = "PrimaryImage";
+            this.PrimaryImage.Size = new System.Drawing.Size(162, 117);
+            this.PrimaryImage.TabIndex = 7;
+            this.PrimaryImage.TabStop = false;
+            // 
+            // SecondaryImage
+            // 
+            this.SecondaryImage.Location = new System.Drawing.Point(97, 262);
+            this.SecondaryImage.Name = "SecondaryImage";
+            this.SecondaryImage.Size = new System.Drawing.Size(162, 117);
+            this.SecondaryImage.TabIndex = 8;
+            this.SecondaryImage.TabStop = false;
+            // 
+            // MeleeImage
+            // 
+            this.MeleeImage.Location = new System.Drawing.Point(97, 456);
+            this.MeleeImage.Name = "MeleeImage";
+            this.MeleeImage.Size = new System.Drawing.Size(162, 117);
+            this.MeleeImage.TabIndex = 9;
+            this.MeleeImage.TabStop = false;
+            // 
+            // ExtraPicture
+            // 
+            this.ExtraPicture.Location = new System.Drawing.Point(97, 623);
+            this.ExtraPicture.Name = "ExtraPicture";
+            this.ExtraPicture.Size = new System.Drawing.Size(162, 117);
+            this.ExtraPicture.TabIndex = 10;
+            this.ExtraPicture.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("TF2 Build", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Yellow;
+            this.label5.Location = new System.Drawing.Point(112, 762);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Watch";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(39)))), ((int)(((byte)(36)))));
+            this.ClientSize = new System.Drawing.Size(347, 855);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ExtraPicture);
+            this.Controls.Add(this.MeleeImage);
+            this.Controls.Add(this.SecondaryImage);
+            this.Controls.Add(this.PrimaryImage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Loadout Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.PrimaryImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondaryImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MeleeImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ExtraPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox PrimaryImage;
+        private System.Windows.Forms.PictureBox SecondaryImage;
+        private System.Windows.Forms.PictureBox MeleeImage;
+        private System.Windows.Forms.PictureBox ExtraPicture;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TF2ClassGenrator/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TF2ClassGenrator
+{
+
+    public partial class Form1 : Form
+    {
+        int Class = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Generate Button
+        {
+            Random random = new Random();
+            Class = random.Next(1, 10);
+            var weaponPath = "tf2Weapons.txt";
+            string[] lines = System.IO.File.ReadAllLines(weaponPath);
+            switch (Class)
+            {
+                case 1:
+                    label1.Text = "Scout";
+                    label2.Text = lines[random.Next(1, 6)];
+                    label3.Text = lines[random.Next(7, 16)];
+                    label4.Text = lines[random.Next(18, 29)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 2:
+                    label1.Text = "Soldier";
+                    label2.Text = lines[random.Next(30, 38)];
+                    label3.Text = lines[random.Next(39, 48)];
+                    label4.Text = lines[random.Next(49, 55)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 3:
+                    label1.Text = "Pyro";
+                    label2.Text = lines[random.Next(56, 62)];
+                    label3.Text = lines[random.Next(63, 71)];
+                    label4.Text = lines[random.Next(72, 83)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 4:
+                    label1.Text = "Demoman";
+                    label2.Text = lines[random.Next(84, 90)];
+                    label3.Text = lines[random.Next(91, 97)];
+                    label4.Text = lines[random.Next(98, 108)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 5:
+                    label1.Text = "Heavy";
+                    label2.Text = lines[random.Next(109, 114)];
+                    label3.Text = lines[random.Next(115, 123)];
+                    label4.Text = lines[random.Next(124, 132)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 6:
+                    label1.Text = "Engineer";
+                    label2.Text = lines[random.Next(133, 138)];
+                    label3.Text = lines[random.Next(139, 144)];
+                    label4.Text = lines[random.Next(145, 150)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 7:
+                    label1.Text = "Medic";
+                    label2.Text = lines[random.Next(151, 154)];
+                    label3.Text = lines[random.Next(155, 158)];
+                    label4.Text = lines[random.Next(159, 163)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 8:
+                    label1.Text = "Sniper";
+                    label2.Text = lines[random.Next(164, 173)];
+                    label3.Text = lines[random.Next(174, 180)];
+                    label4.Text = lines[random.Next(181, 184)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.Visible = false;
+                    label5.Visible = false;
+                    break;
+                case 9:
+                    label5.Visible = true;
+                    ExtraPicture.Visible = true;
+                    label1.Text = "Spy";
+                    label2.Text = lines[random.Next(187, 192)];
+                    label3.Text = lines[random.Next(193, 199)];
+                    label4.Text = lines[random.Next(205, 208)];
+                    label5.Text = lines[random.Next(200, 204)];
+                    PrimaryImage.ImageLocation = "Images/" + label2.Text + ".png";
+                    SecondaryImage.ImageLocation = "Images/" + label3.Text + ".png";
+                    MeleeImage.ImageLocation = "Images/" + label4.Text + ".png";
+                    ExtraPicture.ImageLocation = "Images/" + label5.Text + ".png";
+                    break;
+            }
+
+        }
+
+    }
+}
